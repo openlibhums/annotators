1b74149f10c05a25b04f3b23cd7afe2a92a7b782
Andy Byers <ajrbyers@winterfell.local>
2023-06-23 16:26:25 +0100

    Updated annotators to 1.5.0, removed annotran which is now dead in the water.

--- views.py
+++ views.py
@@ -1,27 +1,26 @@
 from django.shortcuts import render, redirect
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 
 from plugins.annotators import plugin_settings
 from utils import setting_handler
 
+
 def index(request):
-    plugin = plugin_settings.get_self()
+    plugin = plugin_settings.AnnotatorsPlugin.get_self()
 
-    enable_annotran = setting_handler.get_plugin_setting(plugin, 'enable_annotran', request.journal, create=True,
-                                                         pretty='Enable Annotran')
-
-    enable_hypothesis = setting_handler.get_plugin_setting(plugin, 'enable_hypothesis', request.journal, create=True,
-                                                           pretty='Enable Hypothesis')
+    setting_group_name = 'plugin:annotators'
+    enable_hypothesis = setting_handler.get_setting(
+        setting_group_name=setting_group_name,
+        setting_name='enable_hypothesis',
+        journal=request.journal,
+    )
     if request.POST:
-        annotran = request.POST.get('annotran')
         hypothesis = request.POST.get('hypothesis')
-        setting_handler.save_plugin_setting(plugin, 'enable_annotran', annotran, request.journal)
         setting_handler.save_plugin_setting(plugin, 'enable_hypothesis', hypothesis, request.journal)
         return redirect(reverse('annotators_index'))
 
     template = 'annotators/index.html'
     context = {
-        'enable_annotran': enable_annotran,
         'enable_hypothesis': enable_hypothesis,
     }
 
